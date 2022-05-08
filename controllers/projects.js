0765fd2d76a8e16e1fc916781aec0acd72a5bd68
Alex Lord <j.alexlord@gmail.com>
2022-05-08 14:10:30 -0500

    remove promise syntax and replace with async/await - update function

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,9 +1,13 @@
+// require the model to access the DB
 let Project = require("../models/project");
-let nodeGeocoder = require("node-geocoder");
-const Geocoder = require("node-geocoder/lib/geocoder");
+
+
+const NodeGeocoder = require("node-geocoder");
+const geoCoder = NodeGeocoder({ provider: "openstreetmap" });
+// const Geocoder = require("node-geocoder/lib/geocoder");
 
 const request = require("request");
-// file-system package to us get the base64 encoded image
+// file-system package to get the base64 encoded image
 const fs = require("fs");
 
 module.exports = {
@@ -16,36 +20,24 @@ module.exports = {
   delete: deleteProject,
 };
 
-// render all projects page
+// render all projects/index template and send all projects from DB to template
 function index(req, res) {
-  Project.find({}, function (err, projects) {
-    res.render("projects/index", { projects });
-  });
+  Project.find({}, (err, projects) => res.render("projects/index", { projects }));
 }
-// render new project form page
+
+// render new project form template
 function newProject(req, res) {
   res.render("projects/new");
 }
-// render project page
-function show(req, res) {
-  Project.findById(req.params.id, function (err, project) {
-    res.render("projects/show", { project });
-  });
-}
 
-//helper function base_64 encode
-function base64_encode(image) {
-  // read binary data
-  var bitmap = fs.readFileSync(image);
-  // convert binary data to base64 encoded string
-  return bitmap.toString("base64");
+// find project by Id in DB, render project template and send project object to template
+function show(req, res) {
+  Project.findById(req.params.id, (err, project) => res.render("projects/show", { project }));
 }
 
 // create new project
 function create(req, res) {
-  console.log(req.file);
-  console.log(req.body);
-
+  
   let image = base64_encode(req.file.path);
 
   const options = {
@@ -64,7 +56,7 @@ function create(req, res) {
     if (err) return console.log(err);
     let body = JSON.parse(response.body);
 
-    let geoCoder = nodeGeocoder({ provider: "openstreetmap" });
+    let geoCoder = NodeGeocoder({ provider: "openstreetmap" });
     geoCoder
       .geocode({ city: req.body.city, country: "Colombia", limit: 1 })
       .then((res) => {
@@ -97,23 +89,23 @@ function edit(req, res) {
   });
 }
 
-function update(req, res) {
-  let geoCoder = nodeGeocoder({ provider: "openstreetmap" });
-  geoCoder
-    .geocode({ city: req.body.city, country: "Colombia", limit: 1 })
-    .then((res) => {
-      req.body.lat = res[0].latitude;
-      req.body.lon = res[0].longitude;
-    })
-    .then(() => {
-      Project.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        function (err, project) {
-          res.redirect(`/projects/${project.id}`);
-        }
-      );
-    });
+async function update(req, res) {
+    
+    let response = await geoCoder.geocode({ city: req.body.city, country: "Colombia", limit: 1 })
+    // should handle error before continuing
+
+    // save lat and lon from the geocode response in req.body  
+    req.body.lat = response[0].latitude;
+    req.body.lon = response[0].longitude;
+    
+    // find project by Id in DB and update
+    Project.findByIdAndUpdate(
+          req.params.id,
+          req.body,
+          function (err, project) {
+            res.redirect(`/projects/${project.id}`);
+          }
+    );   
 }
 
 function deleteProject(req, res) {
@@ -124,4 +116,15 @@ function deleteProject(req, res) {
     project.remove();
     res.redirect("/projects");
   });
+}
+
+
+/*---- helper functions ----*/
+
+//base_64 encode
+function base64_encode(image) {
+  // read binary data
+  var bitmap = fs.readFileSync(image);
+  // convert binary data to base64 encoded string
+  return bitmap.toString("base64");
 }
